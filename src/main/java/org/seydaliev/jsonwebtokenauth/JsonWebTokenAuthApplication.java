d1a3688f1bac8d6b0155cebaec858ec555be9a67
Seydalievnikolay <118976386+Seydalievnikolay@users.noreply.github.com>
2024-04-22 17:19:29 +0700

    init

---
+++ src/main/java/org/seydaliev/jsonwebtokenauth/JsonWebTokenAuthApplication.java
@@ -0,0 +1,13 @@
+package org.seydaliev.jsonwebtokenauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonWebTokenAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonWebTokenAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/seydaliev/jsonwebtokenauth/JsonWebTokenAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.seydaliev.jsonwebtokenauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonWebTokenAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
